6ece9cca667ecc3e4608ef8a12f3e75b6029155c
Jose Gomez <jose.gomezpereira001@mymdc.net>
2022-10-24 23:08:58 +0000

    Almost done, missing cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,24 +3,36 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+// importing components
+import Navbar from "./navbar.jsx"
+
+const links = [
+	{linkText: 'Home', url: '#'},
+	{linkText: 'About', url: '#'},
+	{linkText: 'Services', url: '#'},
+	{linkText: 'Contact', url: '#'},
+];
+
 //create your first component
 const Home = () => {
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div  className="mx-3">
+			<Navbar links={links}/>
+			<section style={{marginTop: "72px"}}>
+				<div className="container-fluid bg-light mt-3 py-3 px-lg-5">
+					<div className="row">
+						<div className="col col-12">
+							<h1 className="display-1">A Warm Welcome!</h1>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, soluta eum! Provident dolore nostrum placeat amet tempore nesciunt fugit delectus illo eligendi quisquam dignissimos accusantium, aut atque dolorem quae corrupti?</p>
+							<button className="btn btn-primary">Call to action!</button>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
-	);
+		
+	)
 };
 
 export default Home;
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MenuToggler from './menu-toggler.jsx';
+import NavbarMenu from './navbar-menu.jsx';
+
+const Navbar = ({links}) => {
+
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark fixed-top">
+            <div className="container-fluid px-lg-5">
+                <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+                <MenuToggler />
+                <NavbarMenu links={links} />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
