fac4cffebb5d70ec6c66629f65b823db36468e5c
Jose Gomez <jose.gomezpereira001@mymdc.net>
2022-10-25 00:05:30 +0000

    Fixed img

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     return (
         <div className="col-12 col-md-3 mb-3">
             <div className="card">
-                <img src="http://via.placeholder.com/350x150" className="card-img-top" alt="" />
+                <img src="http://via.placeholder.com/500x325" className="card-img-top" alt="" />
                 <div className="card-body">
                     <h5 className="card-title text-center">Card title</h5>
                     <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere id odit assumenda. Ipsum animi sit, assumenda similique corrupti minima deserunt fugiat facere eligendi pariatur nesciunt labore, quo quibusdam, laborum modi.</p>
